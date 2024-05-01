51e1f519a399edafba2174702df6ad07ad953279
BrunoStudi <r.brunocarriere@protonmail.com>
2024-05-01 15:26:21 +0200

    ajout de la gestion des produits et la gestion d'upload d'images (plusieurs, ARRAY)

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
@@ -18,7 +21,14 @@ class CategoryCrudController extends AbstractCrudController
         return Category::class;
     }
 
-    
+    public function configureActions(Actions $actions): Actions 
+    {
+        return $actions
+        ->add(Crud::PAGE_EDIT, Action::INDEX)
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+        ->add(Crud::PAGE_EDIT, Action::DETAIL);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,10 +3,19 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -15,14 +24,42 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
-    /*
+    public function configureActions(Actions $actions): Actions 
+    {
+        return $actions
+        ->add(Crud::PAGE_EDIT, Action::INDEX)
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+        ->add(Crud::PAGE_EDIT, Action::DETAIL);
+    }
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id') -> hideOnForm(),
+            AssociationField::new('categories'),
+            TextField::new('name'),
+            SlugField::new('slug') -> setTargetFieldName('name'),
+            ImageField::new('imageUrls')
+            -> setFormTypeOptions([
+                "multiple" => true,
+                'attr' => [
+                    'accept' => 'image/x-png, image/gif, image/jpeg, image/jpg, image/webp'
+                ]
+            ])
+            -> setBasePath("assets/images/produits")
+            -> setUploadDir("/public/assets/images/produits")
+            -> setUploadedFileNamePattern('[randomhash].[extension]'),
+            TextField::new('description'),
+            TextEditorField::new('more_description'),
+            TextEditorField::new('additionnal_infos'),
+            MoneyField::new('solde_price') -> setCurrency("EUR"),
+            MoneyField::new('regular_price') -> setCurrency("EUR"),
+            IntegerField::new('Stock'),
+            AssociationField::new('categories'),
+            BooleanField::new('isBestSeller'),
+            BooleanField::new('isNewArrival'),
+            BooleanField::new('isFeatured'),
+            BooleanField::new('isSpecialOffer'),
         ];
     }
-    */
 }
